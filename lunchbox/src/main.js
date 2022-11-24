13f45bd1278f9799b950a0e52d14a95355622a5c
Cedricyu <86541569+Cedricyu@users.noreply.github.com>
2022-11-24 14:47:23 +0800

    add lunch box

--- lunchbox/src/main.js
+++
@@ -1,7 +0,0 @@
-// import './plugins/bootstrap-vue'
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-
-createApp(App).use(store).use(router).mount('#app')
