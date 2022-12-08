d4c4b8e4e7c1e1c990fbef33c9ffa6c59f0dce1c
Cedricyu <86541569+Cedricyu@users.noreply.github.com>
2022-12-08 15:45:00 +0800

    lunchbox

--- public/js/lunchbox.js
+++ public/js/lunchbox.js
@@ -5,7 +5,7 @@
 //         $(".xs2").toggle();
 //       });});
 
-const showhealth = () => {
+const getHealth = () => {
     // const data = {
     //     //email: localStorage.getItem('email'),
     //     userId: '1234'
@@ -38,34 +38,3 @@ const showhealth = () => {
         });
 };
 
-const getLunchboxMenu = () => {
-    // const data = {
-    //     //email: localStorage.getItem('email'),
-    //     userId: '1234'
-    // }
-    console.log('enter fondend');
-    const userId = '1234';
-    fetch(`/health?userid=${userId}`, {
-        method: 'get',
-        headers: {
-            'content-type': 'application/json',
-        },
-    })
-        .then(async (response) => {
-            console.log('response', response);
-            if (!response.ok) {
-                const error = await response.text();
-                // document.getElementById('error').innerHTML = 'Error : ' + error;
-            }
-            return response.json();
-        })
-        .then((data) => {
-            console.log(data);
-            // window.alert('成功！');
-            // window.location = '/shop';
-            return data.cal;
-        })
-        .catch((err) => {
-            console.log('err', err);
-        });
-};
